7e9c2d907a6b8061d40c77b94e6bb6a083195c3e
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-05-31 13:37:35 +0530

    Content Updated

--- src/com/devstack/lms/controller/StudentFormController.java
+++ src/com/devstack/lms/controller/StudentFormController.java
@@ -1,4 +1,32 @@
 package com.devstack.lms.controller;
 
+import com.devstack.lms.db.DatabaseAccessCode;
+import com.devstack.lms.model.Student;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+
+import java.util.UUID;
+
 public class StudentFormController {
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtAge;
+    public TextField txtEmail;
+
+    public void saveStudentOnAction(ActionEvent actionEvent) {
+        Student student = new Student(
+                UUID.randomUUID().toString(),
+                txtName.getText().trim(),
+                txtAddress.getText().trim(),
+                txtEmail.getText().toLowerCase().trim(),
+                Integer.parseInt(txtAge.getText())
+        );
+        DatabaseAccessCode databaseAccessCode = new DatabaseAccessCode();
+        boolean isSaved = databaseAccessCode.saveStudent(student);
+        if(isSaved){
+            new Alert(Alert.AlertType.INFORMATION,"Student has been saved...", ButtonType.CLOSE).show();
+        }
+    }
 }
---
+++ src/com/devstack/lms/db/DatabaseAccessCode.java
@@ -0,0 +1,10 @@
+package com.devstack.lms.db;
+
+import com.devstack.lms.model.Student;
+
+public class DatabaseAccessCode {
+    public boolean saveStudent(Student student){
+        //save code
+        return false;
+    }
+}
