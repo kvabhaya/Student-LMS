2086dcecdca09d1368afc4145fbb21cd6d1a0481
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-06-07 17:05:17 +0530

    Rrgistration detail fxml updated

--- src/com/devstack/lms/controller/RegistrationDetailFormController.java
+++ src/com/devstack/lms/controller/RegistrationDetailFormController.java
@@ -1,4 +1,11 @@
 package com.devstack.lms.controller;
 
+import javafx.scene.control.TextField;
+
 public class RegistrationDetailFormController {
+    public TextField courseName;
+    public TextField courseFee;
+    public TextField StudentName;
+    public TextField studentEmail;
+    public TextField registeredDate;
 }
