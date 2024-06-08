c20f6b7f8b6442cb0e6321338657c669f334daae
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-06-08 13:47:04 +0530

    updated

--- src/com/devstack/lms/controller/RegistrationDetailFormController.java
+++ src/com/devstack/lms/controller/RegistrationDetailFormController.java
@@ -1,6 +1,12 @@
 package com.devstack.lms.controller;
 
+import com.devstack.lms.db.DatabaseAccessCode;
+import com.devstack.lms.model.Course;
+import com.devstack.lms.model.Registration;
+import com.devstack.lms.model.Student;
 import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
 
 public class RegistrationDetailFormController {
     public TextField courseName;
@@ -8,8 +14,5 @@ public class RegistrationDetailFormController {
     public TextField StudentName;
     public TextField studentEmail;
     public TextField registeredDate;
-
-
-
 
 }
