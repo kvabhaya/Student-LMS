dd6051de165379c9d24ab46840cfa26ab3296dee
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-06-11 16:50:04 +0530

    convert to layered Architecture

--- src/com/devstack/lms/dao/custom/impl/CourseDaoImpl.java
+++ src/com/devstack/lms/dao/custom/impl/CourseDaoImpl.java
@@ -1,35 +1,83 @@
 package com.devstack.lms.dao.custom.impl;
 
 import com.devstack.lms.dao.custom.CourseDao;
+import com.devstack.lms.db.DbConnection;
 import com.devstack.lms.entity.Course;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class CourseDaoImpl implements CourseDao {
 
     @Override
-    public boolean create(Course course) {
-        return false;
+    public boolean create(Course course) throws SQLException, ClassNotFoundException {
+        String sql = "INSERT INTO course VALUES (?,?,?)";
+        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
+        preparedStatement.setString(1, course.getCourseId());
+        preparedStatement.setString(2, course.getCourseName());
+        preparedStatement.setDouble(3, course.getFee());
+
+
+        return preparedStatement.executeUpdate()>0;
     }
 
     @Override
-    public Course find(String s) {
+    public Course find(String s) throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM course WHERE course_id=?";
+        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
+        preparedStatement.setString(1,s);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if(resultSet.next()){
+            return new Course(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getDouble(3)
+            );
+        }
         return null;
     }
 
     @Override
-    public boolean update(Course course) {
-        return false;
+    public boolean update(Course course) throws SQLException, ClassNotFoundException {
+        String sql = "UPDATE course SET course_name=?, fee=? WHERE course_id=?";
+        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
+
+        preparedStatement.setString(1, course.getCourseName());
+        preparedStatement.setDouble(2, course.getFee());
+        preparedStatement.setString(3, course.getCourseId());
+
+        return preparedStatement.executeUpdate()>0;
     }
 
     @Override
-    public boolean delete(String s) {
-        return false;
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        String sql = "DELETE FROM course WHERE course_id=?";
+        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
+        preparedStatement.setString(1, s);
+
+        return preparedStatement.executeUpdate()>0;
     }
 
     @Override
-    public List<Course> findAll() {
-        return Collections.emptyList();
+    public List<Course> findAll() throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM course";
+        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<Course> courseList = new ArrayList<>();
+        while(resultSet.next()){
+            courseList.add(new Course(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getDouble(3)
+            ));
+        }
+        return courseList;
     }
 }
--- src/com/devstack/lms/db/DatabaseAccessCode.java
+++ src/com/devstack/lms/db/DatabaseAccessCode.java
@@ -10,105 +10,12 @@ public class DatabaseAccessCode {
     //Student
 
     //user
-    public boolean signUp(User user) throws ClassNotFoundException, SQLException {
-        String sql = "INSERT INTO user VALUES (?,?,?)";
-        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
-        preparedStatement.setString(1, user.getUserId());
-        preparedStatement.setString(2, user.getUsername());
-        preparedStatement.setString(3, user.getPassword());
-
-        return preparedStatement.executeUpdate()>0;
-    }
-
-    public boolean login(String username, String password) throws ClassNotFoundException, SQLException {
-        String sql = "SELECT * FROM user WHERE username = '" + username +"' AND password = '" + password +"'";
-        Statement statement = DbConnection.getInstance().getConnection().createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);
-
-        if(resultSet.next()){
-            return true;
-        }
-        return false;
-    }
 
     //course
-    public boolean saveCourse(Course course) throws ClassNotFoundException, SQLException {
-        String sql = "INSERT INTO course VALUES (?,?,?)";
-        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
-        preparedStatement.setString(1, course.getCourseId());
-        preparedStatement.setString(2, course.getCourseName());
-        preparedStatement.setDouble(3, course.getFee());
 
-
-        return preparedStatement.executeUpdate()>0;
-    }
-
-    public List<Course> findAllCourses(String searchText) throws ClassNotFoundException, SQLException {
-        String sql = "SELECT * FROM course";
-        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
-
-        ResultSet resultSet = preparedStatement.executeQuery();
-        List<Course> courseList = new ArrayList<>();
-        while(resultSet.next()){
-            courseList.add(new Course(
-                    resultSet.getString(1),
-                    resultSet.getString(2),
-                    resultSet.getDouble(3)
-            ));
-        }
-        return courseList;
-    }
-
-    public Course findCourse(String course_id) throws ClassNotFoundException, SQLException {
-        String sql = "SELECT * FROM course WHERE course_id=?";
-        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
-        preparedStatement.setString(1,course_id);
-
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-        if(resultSet.next()){
-            return new Course(
-                    resultSet.getString(1),
-                    resultSet.getString(2),
-                    resultSet.getDouble(3)
-            );
-        }
-        return null;
-    }
-
-    public boolean deleteCourse(String courseId) throws SQLException, ClassNotFoundException {
-        String sql = "DELETE FROM course WHERE course_id=?";
-        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
-        preparedStatement.setString(1, courseId);
-
-        return preparedStatement.executeUpdate()>0;
-    }
-
-    public boolean updateCourse(Course course) throws ClassNotFoundException, SQLException {
-        String sql = "UPDATE course SET course_name=?, fee=? WHERE course_id=?";
-        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
-
-        preparedStatement.setString(1, course.getCourseName());
-        preparedStatement.setDouble(2, course.getFee());
-        preparedStatement.setString(3, course.getCourseId());
-
-        return preparedStatement.executeUpdate()>0;
-    }
 
     //Registration
-    public boolean register(Registration registration) throws ClassNotFoundException, SQLException {
-        String sql = "INSERT INTO registration VALUES (?,?,?,?,?,?)";
-        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
-        preparedStatement.setString(1, registration.getRegisterId());
-        preparedStatement.setObject(2, registration.getDate());
-        preparedStatement.setObject(3, registration.getNic());
-        preparedStatement.setString(4, registration.getPaymentType().name());
-        preparedStatement.setString(5, registration.getStudent());
-        preparedStatement.setString(6, registration.getCourse());
 
-
-        return preparedStatement.executeUpdate()>0;
-    }
 
     //All Registration
     public List<AllRegistrations> loadAllDetails(String courseId) throws ClassNotFoundException, SQLException {
